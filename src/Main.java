b5bc727c56d1f2b33d136d84145e4a2ca0f6eb8f
ConstDet <constx7@gmail.com>
2023-02-06 00:33:06 +0300

    Добавлен Main.java в отслеживание изменений

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] sales = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println("Максимальное число в массиве: " + salesManager.max());
     }
 }
