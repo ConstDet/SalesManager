9f6e7baeed9d04f796e4f7eea66b700b5b734fe8
ConstDet <constx7@gmail.com>
2023-02-09 21:43:40 +0300

    Добавлены методы min() и cutAverage()

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int[] sales = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         SalesManager salesManager = new SalesManager(sales);
-        System.out.println("Максимальное число в массиве: " + salesManager.max());
+        System.out.println("Максимальная торговая сделка: " + salesManager.max());
+
+        System.out.println("Усеченное среднее по продажам: " + salesManager.cutAverage());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -13,4 +13,26 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public double cutAverage() {
+        double sumArray = 0;
+        int count = 0, min = this.min(), max = this.max();
+        for (int sale : sales) {
+            if (sale != min && sale != max) {
+                sumArray += sale;
+                count++;
+            }
+        }
+        return sumArray / count;
+    }
 }
